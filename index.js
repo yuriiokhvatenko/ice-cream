31476b5e770d1130e8eee598fe5aa41716228c7e
yuriiokhvatenko <113927739+yuriiokhvatenko@users.noreply.github.com>
2022-12-26 13:09:02 +0200

    copying from team projects

---
+++ index.js
@@ -0,0 +1,8 @@
+import './js/swiper-slider';
+import './js/modal';
+import './js/gallary-slider';
+import './js/mobile-menu';
+import './js/snow3d';
+import './js/mobile-menu-effect';
+import './js/num';
+import './js/eyes';
